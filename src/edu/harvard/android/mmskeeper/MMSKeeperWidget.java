ede5c2c2e307a45b9f9ba179585d373a48935a5e
bencebeky <zsebkecske@gmail.com>
2014-01-28 16:31:42 -0500

    GPLv3+ Copyright added

--- src/edu/harvard/android/mmskeeper/MMSKeeperWidget.java
+++ src/edu/harvard/android/mmskeeper/MMSKeeperWidget.java
@@ -1,3 +1,23 @@
+/*
+    MMSKeeper Android app: switches data on/off while retaining MMS traffic.
+    Copyright (C) 2014 Bence Béky
+
+    This file is part of MMSKeeper.
+
+    MMSKeeper is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MMSKeeper is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MMSKeeper.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package edu.harvard.android.mmskeeper;
 
 import android.app.PendingIntent;
